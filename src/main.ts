32bdce3dd1c24a0aba4d492966776624402cfcb5
osydorchuk89 <oleksii.sydorchuk@gmail.com>
2025-06-29 11:57:39 +0200

    removed pinia leftovers

--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,11 @@
 import "./assets/main.css";
-import "./assets/shared.css"
+import "./assets/shared.css";
 import FontAwesomeIcon from "@/lib/fontawesome-icons";
 
 import { createApp } from "vue";
-import { createPinia } from "pinia";
 import App from "./App.vue";
 
 const app = createApp(App);
-
-app.use(createPinia());
 
 app.component("font-awesome-icon", FontAwesomeIcon);
 app.mount("#app");
