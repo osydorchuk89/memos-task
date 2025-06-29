20ebd60c831c02d11ed4ebd19f7912b11559aaaf
osydorchuk89 <oleksii.sydorchuk@gmail.com>
2025-06-29 23:02:57 +0200

    updated deferred loading

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,26 @@
 <script setup lang="ts">
+import { onMounted, ref } from "vue";
+
 import ActionSection from "./components/sections/action/ActionSection.vue";
 import ContactSection from "./components/sections/contact/ContactSection.vue";
 import FeaturesSection from "./components/sections/features/FeaturesSection.vue";
 import TheFooter from "./components/sections/footer/TheFooter.vue";
 import HomeSection from "./components/sections/home/HomeSection.vue";
 import PricesSection from "./components/sections/prices/PricesSection.vue";
+import homeImageUrl from "./assets/images/home-image.webp";
+
+const imageLoaded = ref(false);
+onMounted(() => {
+    const img = new Image();
+    img.onload = () => {
+        imageLoaded.value = true;
+    };
+    img.src = homeImageUrl;
+});
 </script>
 
 <template>
-    <main class="scroll-container">
+    <main v-if="imageLoaded" class="scroll-container">
         <HomeSection class="scroll-section" />
         <FeaturesSection class="scroll-section" />
         <ActionSection class="scroll-section" />
@@ -16,6 +28,7 @@ import PricesSection from "./components/sections/prices/PricesSection.vue";
         <PricesSection class="scroll-section" />
         <TheFooter class="footer-section" />
     </main>
+    <div v-else="loading"></div>
 </template>
 
 <style scoped>
@@ -37,5 +50,10 @@ import PricesSection from "./components/sections/prices/PricesSection.vue";
     height: auto;
     scroll-snap-align: end;
     scroll-snap-stop: normal;
+}
+
+.loading {
+    height: 100vh;
+    overflow: hidden;
 }
 </style>
--- src/components/sections/home/HomeSection.vue
+++ src/components/sections/home/HomeSection.vue
@@ -1,25 +1,11 @@
 <script setup lang="ts">
-import { onMounted, ref } from "vue";
-
 import HomeCarousel from "./HomeCarousel.vue";
 import BaseButton from "@/components/ui/BaseButton.vue";
 import HomeNavBar from "./HomeNavBar.vue";
-import homeImageUrl from "../../../assets/images/home-image.webp";
-
-const imageLoaded = ref(false);
-const imageUrl = `url(${homeImageUrl})`;
-
-onMounted(() => {
-    const img = new Image();
-    img.onload = () => {
-        imageLoaded.value = true;
-    };
-    img.src = homeImageUrl;
-});
 </script>
 
 <template>
-    <section v-if="imageLoaded" id="home" class="home-container bg-section-base section-centered">
+    <section id="home" class="home-container bg-section-base section-centered">
         <HomeNavBar />
         <HomeCarousel />
         <BaseButton
@@ -28,15 +14,10 @@ onMounted(() => {
             active-color="var(--secondary-active)"
         />
     </section>
-    <section v-else class="section-loading" />
 </template>
 
 <style scoped>
 .home-container {
-    background-image: v-bind(imageUrl);
-}
-
-.section-loading {
-    height: 100vh;
+    background-image: url("../../../assets/images/home-image.webp");
 }
 </style>
