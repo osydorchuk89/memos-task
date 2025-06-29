2ed235addd89a90f483f47a26be791101dc77a31
osydorchuk89 <oleksii.sydorchuk@gmail.com>
2025-06-29 22:48:15 +0200

    delayed content loading

--- src/components/sections/home/HomeSection.vue
+++ src/components/sections/home/HomeSection.vue
@@ -1,18 +1,25 @@
 <script setup lang="ts">
-import { onMounted } from "vue";
+import { onMounted, ref } from "vue";
 
 import HomeCarousel from "./HomeCarousel.vue";
 import BaseButton from "@/components/ui/BaseButton.vue";
 import HomeNavBar from "./HomeNavBar.vue";
+import homeImageUrl from "../../../assets/images/home-image.webp";
+
+const imageLoaded = ref(false);
+const imageUrl = `url(${homeImageUrl})`;
 
 onMounted(() => {
     const img = new Image();
-    img.src = "../../../assets/images/home-image.webp";
+    img.onload = () => {
+        imageLoaded.value = true;
+    };
+    img.src = homeImageUrl;
 });
 </script>
 
 <template>
-    <section id="home" class="home-container bg-section-base section-centered">
+    <section v-if="imageLoaded" id="home" class="home-container bg-section-base section-centered">
         <HomeNavBar />
         <HomeCarousel />
         <BaseButton
@@ -21,10 +28,15 @@ onMounted(() => {
             active-color="var(--secondary-active)"
         />
     </section>
+    <section v-else class="section-loading" />
 </template>
 
 <style scoped>
 .home-container {
-    background-image: url("../../../assets/images/home-image.webp");
+    background-image: v-bind(imageUrl);
+}
+
+.section-loading {
+    height: 100vh;
 }
 </style>
